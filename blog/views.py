6637628bb3d2a44434b5644a7905f45d75079aee
rdshinde <rdshinde2000@gmail.com>
2020-12-20 14:36:19 +0530

    Added HTML

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, '/Users/rdshinde/Documents/Django/mysite/blog/template/blog/post_list.html', {})
